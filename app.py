d76b79c4cde1246eb985edb482be00e791ada9fd
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-16 02:45:47 +0200

    make search dynamic

--- app.py
+++ app.py
@@ -1,45 +1,26 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from func import get_google_data
 
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+def search():
+    search_text = st.text_input(
+        'Search Label',
+        '( TITLE ( "MLOps" ) OR ABS ( "MLOps" ) AND LANGUAGE ( english ) OR TITLE ( "Machine Learning DevOps" ) OR ABS ( "Machine Learning DevOps" ) OR TITLE ( "ML-Ops" ) OR ABS ( "ML-Ops" ) OR TITLE ( "ML-DevOps" ) OR ABS ( "ML-DevOps" ) ) AND PUBYEAR > 2014 AND ( LIMIT-TO ( DOCTYPE , "cp" ) OR LIMIT-TO ( DOCTYPE , "ar" ) )',
+        label_visibility='collapsed'
+    )
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    data = get_google_data(search_text)
 
-
+    for article in data:
+        st.subheader(f"[{article['title']}]({article['link']})")
+        st.markdown(f"""
+            *Authors:* {' - '.join(article['authors'])} | *Citations:* {article['citations_number']}
+        """)
+        st.write(article["abstract"])
+        pdf_download_button = st.button("Download PDF", key=f"download_button_{article['title']}")
 
 st.header('My Search', divider='rainbow')
 
-search_text = st.text_input(
-    'search label',
-    '( TITLE ( "MLOps" ) OR ABS ( "MLOps" ) AND LANGUAGE ( english ) OR TITLE ( "Machine Learning DevOps" ) OR ABS ( "Machine Learning DevOps" ) OR TITLE ( "ML-Ops" ) OR ABS ( "ML-Ops" ) OR TITLE ( "ML-DevOps" ) OR ABS ( "ML-DevOps" ) ) AND PUBYEAR > 2014 AND ( LIMIT-TO ( DOCTYPE , "cp" ) OR LIMIT-TO ( DOCTYPE , "ar" ) )',
-    label_visibility='collapsed'
-    )
-
-
-sample_data = {
-    "title": "Mlp-mixer: An all-mlp architecture for vision",
-    "abstract": "This is a sample abstract for the MLP-Mixer paper.",
-    "authors": ["IO Tolstikhin", "N Houlsby", "A Kolesnikov"],
-    "citations_number": 1657,
-    "year": 2021,
-    "link": "https://proceedings.neurips.cc/paper/2021/hash/cba0a4ee5ccd02fda0fe3f9a3e7b89fe-Abstract.html",
-    "pdf_link": "https://proceedings.neurips.cc/paper/2021/file/cba0a4ee5ccd02fda0fe3f9a3e7b89fe-Paper.pdf",
-}
-
-
-st.subheader(f"[{sample_data["title"]}]({sample_data["link"]})")
-st.markdown(f"""
-    *Authors:* {' - '.join(sample_data['authors'])} | *Citations:* {sample_data['citations_number']}
-    """)
-st.write(sample_data["abstract"])
-pdf_download_button = st.button("Download PDF")
+search()
--- func.py
+++ func.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup, SoupStrainer
-
+from util import Article
 
 def get_page(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.72 Safari/537.36'}
@@ -21,47 +21,61 @@ def get_page(url):
     return page
 
 
-page = get_page('https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=mlp&oq=')
+def get_google_data(q):
+    data = []
+
+    page = get_page(f'https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q={q}&oq=')
+
+    articles = page.find_all('div', class_='gs_or')
+    article_div = [link for link in articles]
+
+    for article in articles:
+        # get title
+        h3_element = article.find('h3', class_='gs_rt')
+        title = h3_element.a.text
+
+        # get authors
+        author_a = article.find_all('div', class_='gs_a')
+        author = [author_div.text.strip() for author_div in author_a]
+        authors = author[0].replace("\xa0", '').split("-")
+        authors.pop()
+        authors = authors[0].split(",")
+
+        # get abstract
+        abstract_div = article.find('div', class_='gs_rs')
+        abstract = abstract_div.text
+
+        # get link
+        link = h3_element.a['href']
+
+        # get pdf link
+        pdf_div = article.find('div', class_='gs_or_ggsm')
+        pdf_link = ''
+
+        if pdf_div is not None:
+            pdf_link = pdf_div.a['href']
+
+        # citation number
+        citation_div = article.find('div', class_='gs_flb')
+        citation_list = [link.text for link in citation_div]
+        if len(citation_list) >=4:
+            if len(citation_list[4].split(" ")) >= 2:
+                citation_number = citation_list[4].split(" ")[2]
+            else:
+                citation_number = ''
+        else:
+            citation_number = ''
 
 
-articles = page.find_all('div', class_='gs_or')
-article_div = [link for link in articles]
+        data.append(
+            Article(
+                title = title,
+                abstract = abstract,
+                authors = authors,
+                citations_number = citation_number,
+                year = 2004,
+                link = link
+            ).to_json()
+        )
 
-for article in articles:
-    # get title
-    h3_element = article.find('h3', class_='gs_rt')
-    title = h3_element.a.text
-
-    # get authors
-    author_a = article.find_all('div', class_='gs_a')
-    author = [author_div.text.strip() for author_div in author_a]
-    authors = author[0].replace("\xa0", '').split("-")
-    authors.pop()
-    authors = authors[0].split(",")
-
-    # get abstract
-    abstract_div = article.find('div', class_='gs_rs')
-    abstract = abstract_div.text
-
-    # get link
-    link = h3_element.a['href']
-
-    # get pdf link
-    pdf_div = article.find('div', class_='gs_or_ggsm')
-    pdf_link = ''
-
-    if pdf_div is not None:
-        pdf_link = pdf_div.a['href']
-
-    # citation number
-    citation_div = article.find('div', class_='gs_flb')
-    citation_list = [link.text for link in citation_div]
-    citation_number = citation_number[4].split(" ")[2]
-
-    # print(title)
-    # print(authors)
-    # print(abstract)
-    # print(link)
-    # print(pdf_link)
-    # print(citation_number)
-    print("-" * 20)
+    return data
