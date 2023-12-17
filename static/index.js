00969c46f4bce587ebadc69d0797bef63aecf04c
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-17 20:20:46 +0200

    render data

--- static/index.js
+++ static/index.js
@@ -1,7 +1,39 @@
+function ArticleLayout(
+  title,
+  abstract,
+  authors,
+  citations_number,
+  year,
+  link,
+  pdf_link
+) {
+  const writers = authors?.map((user, index) => {
+    return `&nbsp;${user}`;
+  });
+
+  return `
+    <div class="article">
+        <div class="flex-row article-header">
+        <a href=${link} target="_blank"><h2>${title}</h2></a>
+        <button>PDF</button>
+        </div>
+        <div class="flex-row article-info">
+        <i>Authors:</i> &nbsp; ${writers} &nbsp;| &nbsp;<i>Citations:</i> &nbsp; ${citations_number}
+        </div>
+
+        <div class="article-abstract">
+        ${abstract}
+        </div>
+    </div>
+    `;
+}
+
 const search_input = document.getElementById("q_text");
 const search_btn = document.getElementById("q_button");
+const results_form = document.getElementById("results");
 
 search_btn.addEventListener("click", async () => {
+  let articles = [];
   const q = search_input.value;
 
   if (q.length === 0) {
@@ -20,8 +52,22 @@ search_btn.addEventListener("click", async () => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      articles = data;
     })
     .catch((error) => {
       console.error("Error:", error);
     });
+
+  articles.map((art) => {
+    results_form.innerHTML += ArticleLayout(
+      art.title,
+      art.abstract,
+      art.authors,
+      art.citations_number,
+      // art.year,
+      2004,
+      art.link,
+      art.pdf_link
+    );
+  });
 });
